345757c7958353ad149d7936e285bae8bd8339fc
Dyluvian <152575690+Dyluvian@users.noreply.github.com>
2024-03-01 20:29:24 +0000

    array, setup of menu

--- src/ca2/EmployeeTest.java
+++ src/ca2/EmployeeTest.java
@@ -24,3 +24,31 @@ public class EmployeeTest { // "Write another class, EmployeeTest..."
         projectGroup[2] = employee3;
 
         System.out.println(Employee.nextEmpNum); // "...and prints out the value of variable nextEmpNum to the terminal window." (and JUST the value as prescribed)
+
+// "Write the code to search and display the name of each of the employees in the projectGroup array, who have an employee number above the value stored in a variable called m."
+        Random r = new Random();
+        int m = r.nextInt(2); // no value has been prescribed for m, thus for these purposes just generate something random to ensure at least one name displays
+
+        for (Employee projectmember : projectGroup) { // "search the name of each of the employees in the projectGroup array..."
+            if (projectmember.empNum > m) { // "...who have an employee number above the value stored in a variable called m..."
+                System.out.println(projectmember.name); // "...and display"
+            }
+        }
+
+// "Create a Netbean-console menu system for the manager to log in and view current staff and add new staff. (username – “Gnomeo”; Password – “smurf”)"
+
+/*/ Although not explicitly obligated, I went for a looping menu in which the manager can repeatedly perform all operations in one session. Hopefully nothing breaks or softlocks. /*/
+
+        Manager[] managerGroup = new Manager[1]; // create a group for managers, making this menu system flexible beyond Gnomeo
+
+        managerGroup[0] = new Manager("Waylon Smithers", "ws@gmail.com", "Gnomeo", "smurf"); // create the Gnomeo manager
+
+        Company BusinessGnómes = new Company("Business Gnómes"); // create the Business Gnómes company
+
+        boolean login = false; // boolean for user logged out or user logged in
+
+        String inputUsername = null; // initialise username early for printing
+
+        BusinessGnómes.staff.addAll(Arrays.asList(projectGroup)); // load all previously defined employees into the company array list
+        BusinessGnómes.staff.addAll(Arrays.asList(managerGroup)); // load all previously defined managers into the company array list
+
