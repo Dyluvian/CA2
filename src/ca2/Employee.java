4605091e191da0d9a6d06fea123dd310798b0306
Dyluvian <152575690+Dyluvian@users.noreply.github.com>
2024-03-01 20:17:03 +0000

    methods

--- src/ca2/Employee.java
+++ src/ca2/Employee.java
@@ -22,3 +22,37 @@ class Employee { // "Write the complete class, Employee, which has the following
         this.email = email;
         empNum = nextEmpNum++; // "...incremented to give consecutive numbers to each newly created object."
     }
+
+// "Methods:"
+    public String getName() { // "Accessor method to return name value"
+        return this.name;
+    }
+
+    public String getEmail() { // "Accessor method to return email value"
+        return this.email;
+    }
+
+    public int getEmpNum() { // "Accessor method to return empNum value"
+        return this.empNum;
+    }
+
+    public void setEmail(String newEmail) { // "to set the email field to a new value passed as a parameter."
+        if (newEmail.length() <= 3) { // "The value must not be accepted if its length is 3 or less."
+            System.out.println("---\nUnfortunately, " + newEmail + " is not an acceptable email address. The address cannot consist of 3 characters or less.");
+/*/ ----------------------------------------------------
+"Challenge: Implement a check for a valid email address"
+This was a real rabbit hole. There are many varied perspectives on email validation. Who to trust?
+The regex I opted for below, rightly or wrongly, is the RFC 5322 Official Standard as defined by general consensus here: https://emailregex.com/
+I couldn't come up with the headspace to manually review this pattern and painstakingly adapt it to Java syntax myself, so I ran it through this and hoped for the best: https://www.regexplanet.com/advanced/java/index.html
+---------------------------------------------------- /*/
+        } else if (!newEmail.matches("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")) { // Lovecraftian email regex checker
+            System.out.println("---\nUnfortunately, " + newEmail + " is not an acceptable email address. The address must be compliant with the RFC 5322 Official Standard.");
+        } else {
+            this.email = newEmail; // if the input satisfies both checks, set the email
+        }
+    }
+
+    public int getNextEmpNum() { // "to return the current value of nextEmpNum"
+        return this.nextEmpNum;
+    }
+}
